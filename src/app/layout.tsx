8c7e876773b233bd96e33cbbde2bf229e135102c
maxinelobato <barretolobato.org@gmail.com>
2023-06-01 11:21:15 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout ({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'GTM-5D65SH3');
+            })(window,document,'script','dataLayer', 'GTM-KCTZS5L');
           `
           }}
         ></Script>
@@ -39,7 +39,7 @@ export default function RootLayout ({
         <Suspense>
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=GTM-5D65SH3`}
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-KCTZS5L`}
               height='0'
               width='0'
               style={{ display: 'none', visibility: 'hidden' }}
